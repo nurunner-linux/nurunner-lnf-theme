0f1ec3391253d695e54ac041619bfd796089365f
Matthew Kowal <matt@mkxt.net>
2016-09-12 16:40:54 -0400

    fix missing icon bug

--- usr/share/plasma/look-and-feel/org.kde.maui.desktop/contents/plasmoidsetupscripts/org.kde.plasma.kickoff.js
+++ usr/share/plasma/look-and-feel/org.kde.maui.desktop/contents/plasmoidsetupscripts/org.kde.plasma.kickoff.js
@@ -2,7 +2,7 @@ applet.currentConfigGroup = ["Shortcuts"]
 applet.writeConfig("global", "Alt+F1")
 
 applet.currentConfigGroup = ["General"]
-applet.writeConfig("icon", "/usr/share/icons/hicolor/scalable/apps/homerun.svg")
+//applet.writeConfig("icon", "/usr/share/icons/hicolor/scalable/apps/homerun.svg")
 applet.writeConfig("favorites", ["systemsettings.desktop","synaptic.desktop","org.kde.ksysguard.desktop"])
 
 applet.reloadConfig();
